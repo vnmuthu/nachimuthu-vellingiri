8c6028f474b4499543f27effd27831d035c6d840
Nachimuthu Vellingiri <vnmuthu@gmail.com>
2021-02-13 15:12:00 +0530

    Updated Content
    Updated Content

--- meet-muthu/pages/index.js
+++ meet-muthu/pages/index.js
@@ -4,60 +4,49 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>meet-muthu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Nachimuthu <a href="#">Vellingiri</a>
         </h1>
 
+        <h2>Senior Frontend Developer</h2>
+
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>A creative and dependable frontend Developer with 10+ years industry experience in building responsive web applications. A solution-driven professional excelling in highly collaborative work environment, finding solutions to challenges and focused on customer satisfaction.</code>
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="#" className="card">
+            <h3>Work Experience &rarr;</h3>
+            <p>Find my professional experience</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="#" className="card">
+            <h3>Portofolio &rarr;</h3>
+            <p>Find my contributions to brands</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="#"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Testimonials &rarr;</h3>
+            <p>Testimonials</p>
+          </a>
+
+          <a
+            href="#"
+            className="card"
+          >
+            <h3>Accompliments &rarr;</h3>
+            <p>Awards</p>
           </a>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
